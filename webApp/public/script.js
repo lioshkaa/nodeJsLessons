1edc9fd61d7db2333aac891cd7f0ce80d77a12a0
Lesha <89130863+lioshkaa@users.noreply.github.com>
2021-12-17 18:38:38 +0300

    Add files via upload

---
+++ webApp/public/script.js
@@ -0,0 +1,133 @@
+
+
+async function GetUsers(){
+    const response=await fetch("/api/users",{
+        method:"GET",
+        headers:{"Accept":"application/json"}
+    });
+    if(response.ok==true){
+        const users=await response.json();
+        let rows=document.querySelector("tbody")
+        users.forEach(element => {
+           rows.append(row(element)) 
+        });
+    }
+}
+async function GetUsers(id){
+    const response=await fetch("/api/users/"+ id,{
+        method:"GET",
+        headers:{"Accept":"application/json"}
+    });
+    if(response.ok==true){
+        const user =await response.json();
+        const form=document.forms["userForm"];
+        form.elements["id"].value = user.id;
+        form.elements["name"].value = user.name;
+        form.elements["age"].value = user.age;
+    }
+}
+async function CreateUser(userName,userAge){
+    const response=await fetch("api/users",{
+        method:"POST",
+        headers:{"Accept":"application/json","Content-Type":"application/json"},
+        body:JSON.stringify({
+            name:userName,
+            age:parseInt(userAge,10)
+        })
+    });
+    if(response.ok==true){
+        const user=await response.json();
+        reset();
+        document.querySelector("tbody").append(row(user));
+    }
+}
+async function EditUser(userId,userName,userAge){
+    const response=await fetch("api/users",{
+        method:"PUT",
+        headers:{"Accept":"application/json","Content-Type":"application/json"},
+        body:JSON.stringify({
+            id:userId,
+            name:userName,
+            age:parseInt(userAge,10)
+        })
+    });
+    if(response.ok==true){
+        const user=await response.json();
+        reset();
+        document.querySelector("tr[data-rowid="+user.id+ "]").replaceWith(row(user));
+    }
+}
+async function Deleteuser(id){
+    const response=await fetch("/api/users/"+id,{
+        method:"DELETE",
+        headers:{"Accept":"application/json"}
+    });
+    if(response.ok==true){
+        const user=await response.json();
+        document.querySelector("tr[data-rowid="+user.id+"]").remove();
+    }
+}
+function reset(){
+    const form=document.forms["userForm"];
+    form.reset();
+    form.elements["id"].value=0;
+}
+function row(){
+    const tr=document.createElement("td");
+    tr.setAttribute("data-rowid",user.id);
+    const idTd=document.createElement("td");
+    idTd.append(user.id);
+    tr.append(idTd);
+
+    const nameId=document.createElement("td");
+    nameTd.append(user.name);
+    tr.append(nameTd);
+
+    const ageTd=document.createElement("td");
+    ageTd.append(user.age);
+    tr.append(ageTd);
+
+    const linksTd=document.createElement("td");
+
+    const editLink=document.createElement("a");
+    editLink.setAttribute("data-id",user.id);
+    editLink.setAttribute("style","cursor:pointer; padding:15px");
+    editLink.append("Изменить");
+    editLink.addEventListener("click",e=>{
+        e.preventDefault();
+        GetUsers(user.id);
+    });
+    linksTd.append(editLink);
+
+    const removeLink=document.createElement("a");
+    removeLink.setAttribute("data-id",user.id);
+    removeLink.setAttribute("style","cursor:pointer;padding:15px");
+    removeLink.append("Удалить");
+    removeLink.addEventListener("click",e=>{
+        e.preventDefault();
+        Deleteuser(user.id);
+    });
+    linksTd.append(removeLink);
+    tr.appendChild(linksTd);
+
+    return tr;
+}
+document.getElementById("reset").click(e=>{
+    e.preventDefault();
+    reset();
+});
+document.forms["userForm"].addEventListener("submit",e=>{
+    e.preventDefault();
+    const form=document.form["userForm"];
+    const id=form.elements["id"].value;
+    const name=form.elements["name"].value;
+    const age=form.elements["age"].value;;
+
+    if(id==0){
+        CreateUser(name,age);
+    }
+    else{
+        EditUser(id,name,age);
+    }
+});
+GetUsers();
